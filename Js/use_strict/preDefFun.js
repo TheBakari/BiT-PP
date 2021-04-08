14d433fc34caded46f07281ab25af120ff7039ca
TheBakari <draganic69@gmail.com>
2021-04-08 17:57:59 +0200

    update

--- Js/use_strict/preDefFun.js
+++ Js/use_strict/preDefFun.js
@@ -56,6 +56,27 @@ function filter(a){
 console.log(filter([NaN, 0, 15, false, -22, '', undefined, 47, null]))
 
 
+// 4.	Write a program that calculates a number of integer values in the array.
+// 	Input: [NaN, 23.1, 15, false, -22.5, '', 4, 7, null]
+//   Output: 3
+
+var array=[NaN, 23.1, 15, false, -22.5, '', 4, 7, null];
+
+
+function integerNumber(array) {
+  var counter = 0;
+  for (i = 0; i < array.length; i++) {
+    var intNumber = parseInt(array[i]);
+    if (isFinite(intNumber) && parseFloat(array[i]) === intNumber){
+      counter++;
+    } 
+      
+
+  }
+  return counter;
+}
+console.log(integerNumber(array));
+
 // 5. Write a program that calculates a number of float values in the array.
 // Input: [NaN, 23.1, 15, false, -22.5, " ", 4, 7, null]
 // Output: 2
